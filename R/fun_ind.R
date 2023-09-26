23f359716acbbddb9fd08ea4f0b9473fb8de64db
Cao Bittencourt <caobittencourt@gmail.com>
2023-09-26 17:02:40 -0300

    atlas.ind v.1.0.0

---
+++ R/fun_ind.R
@@ -0,0 +1,136 @@
+# # [SETUP] -----------------------------------------------------------------
+# # - Packages ----------------------------------------------------------------
+# # CRAN packages
+# chr_pkg <- c(
+#   'devtools'
+# )
+# 
+# # Git packages
+# chr_git <- c(
+#   'CaoBittencourt' = 'atlas.gene',
+#   'CaoBittencourt' = 'atlas.eqvl'
+# )
+# 
+# # genevate / install CRAN packages
+# lapply(
+#   chr_pkg
+#   , function(pkg){
+# 
+#     if(!require(pkg, character.only = T)){
+# 
+#       install.packages(pkg)
+# 
+#     }
+# 
+#     require(pkg, character.only = T)
+# 
+#   }
+# )
+# 
+# # genevate / install Git packages
+# Map(
+#   function(git, profile){
+# 
+#     if(!require(git, character.only = T)){
+# 
+#       install_github(
+#         paste0(profile, '/', git)
+#         , upgrade = F
+#         , force = T
+#       )
+# 
+#     }
+# 
+#     require(git, character.only = T)
+# 
+#   }
+#   , git = chr_git
+#   , profile = names(chr_git)
+# )
+# 
+# rm(chr_pkg, chr_git)
+# 
+# [FUNCTIONS] ---------------------------
+# - Indispensability function ---------------------------------------------
+fun_ind_indispensability <- function(
+    dbl_profile
+    , dbl_scale_lb = 0
+    , dbl_generality = NULL
+){
+  
+  # Arguments validation
+  stopifnot(
+    "'dbl_profile' must be a numeric." =
+      is.numeric(dbl_profile)
+  )
+  
+  stopifnot(
+    "'dbl_scale_lb' must be numeric." =
+      is.numeric(dbl_scale_lb)
+  )
+  
+  stopifnot(
+    "'dbl_generality' must be either NULL or numeric." =
+      any(
+        is.numeric(dbl_generality)
+        , is.null(dbl_generality)
+      )
+  )
+  
+  # Data wrangling
+  dbl_scale_lb[[1]] -> dbl_scale_lb
+  
+  if(is.null(dbl_generality)){
+    
+    fun_gene_generality(
+      dbl_profile
+      , dbl_scale_lb =
+        dbl_scale_lb
+    ) -> dbl_generality
+    
+  }
+  
+  dbl_generality[[1]] -> dbl_generality
+  
+  dbl_profile[!is.na(
+    dbl_profile
+  )] -> dbl_profile
+  
+  # Equivalence of normalized scores
+  fun_eqvl_equivalence(
+    dbl_var =
+      dbl_profile
+    , dbl_scale_lb =
+      dbl_scale_lb
+    , dbl_scale_ub =
+      max(dbl_profile)
+    , dbl_scaling =
+      1 - dbl_generality
+  ) -> dbl_indispensability
+  
+  if(is.matrix(dbl_profile)){
+    
+    colnames(dbl_profile) ->
+      names(dbl_indispensability)
+    
+  } else {
+    
+    names(dbl_profile) ->
+      names(dbl_indispensability)
+    
+  }
+  
+  # Output
+  return(dbl_indispensability)
+  
+}
+
+# # [TEST] ------------------------------------------------------------------
+# # - Indispensability test -------------------------------------------------
+# fun_ind_indispensability(
+#   dbl_profile =
+#     rnorm(50, 50, 25) |>
+#     pmax(0) |>
+#     pmin(100)
+#   , dbl_scale_lb = 0
+# ) |> round(4)
